333196171ab4b5621b8870c3a7c73b2486f321ea
isayakmondal <isayakmondal@gmail.com>
2022-07-02 19:34:39 +0530

    Initial Commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Header = () => (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      whileHover={{ scale: 1.01 }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋🏼</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hey, I am</p>
+            <h1 className="head-text">SAYAK!</h1>
+          </div>
+        </div>
+
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Web Developer,</p>
+          <p className="p-text">Undergrad CS Student</p>
+        </div>
+      </div>
+    </motion.div>
+
+    <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      whileHover={{ scale: 0.99 }}
+      className="app__header-img"
+    >
+      <img src={images.profile} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
+        src={images.circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
+
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      // whileHover={{ scale: 1.01 }}
+      className="app__header-circles"
+    >
+      {[images.react, images.cpp, images.node].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+  </div>
+);
+
+export default AppWrap(Header, 'home');
